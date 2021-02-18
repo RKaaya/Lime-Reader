f6018e6b356777dba7d8df8408e49878cf244ecf
Rkaaya <gergelynagy99@gmail.com>
2021-02-18 23:09:09 +0100

    init

---
+++ src/main/java/com/rkaaya/limereader/LimeReaderApplication.java
@@ -0,0 +1,13 @@
+package com.rkaaya.limereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimeReaderApplication.class, args);
+    }
+
+}
